4f7ac0d4aa52a121bc0a6ddb6c121ab8e050ad3e
Rebecca Turner <637275@gmail.com>
2018-08-23 21:54:29 -0400

    idk changes i guess; COURSE_LISTS -> COURSES and it's a map now

--- course_science.py
+++ course_science.py
@@ -1,37 +1,53 @@
-from typing import List
+from typing import List, Mapping
 from glob import glob
 import os.path
+from collections import Counter
 
 import brandeis
 
-COURSE_LISTS = []
+COURSES = {}
 
-def all_courses() -> List[List[brandeis.Course]]:
-    global COURSE_LISTS
-    if COURSE_LISTS:
-        return COURSE_LISTS
+def all_courses() -> Mapping[str, List[brandeis.Course]]:
+    global COURSES
+    if COURSES:
+        return COURSES
     else:
-        COURSE_LISTS = read_all()
-        return COURSE_LISTS
+        COURSES = read_all()
+        return COURSES
 
 def read(fname: str) -> List[brandeis.Course]:
-    """for initializing COURSE_LISTS"""
+    """for initializing COURSES"""
     with open(fname, 'r') as f:
         return brandeis.load_courses(f)
 
 def read_all(outdir: str = 'out') -> List[brandeis.Course]:
-    """for initializing COURSE_LISTS"""
+    """for initializing COURSES"""
 
     pat = os.path.join(outdir, '*-*.json')
-    ret = []
+    ret = {}
     # theyre numbered so this works
     for fname in sorted(glob(pat)):
         base, *_ = os.path.basename(fname).split(os.path.extsep)
         year, semester = base.split('-')
-        if semester == '2':
-            # summer
+        if semester == '1':
+            # january
+            semester = '01'
+        elif semester == '2':
+            # skip summer
             continue
-        ret.append(read(fname))
+        elif semester == '3':
+            # september
+            semester = '09'
+        else:
+            raise ValueError('Invalid semester number ' + semester)
+        ret[f'{year}-{semester}'] = read(fname)
+
+    return ret
+
+def courses_per_subject():
+    ret = {}
+    for sem, courses in all_courses().items():
+        ret[sem] = Counter(map(lambda c: c.subject, courses))
 
     return ret
 
@@ -39,7 +55,8 @@ def courses_per_semester() -> List[int]:
     return list(map(len, all_courses()))
 
 def main():
-    print(courses_per_semester())
+    for sem, courses in courses_per_subject().items():
+        print(sem, ':', courses.most_common(5))
 
 if __name__ == '__main__':
     main()
